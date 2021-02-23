17c8e71f5d3942f52499381fc21f2d8b5a061011
ScreamPunk27 <69679651+ScreamPunk27@users.noreply.github.com>
2021-02-23 19:29:54 -0300

    Add files via upload

---
+++ contenidojs.js
@@ -0,0 +1,16 @@
+// let menu = document.querySelector(".lista-menu");
+// let boton = document.querySelector(".botonazo");
+// let cerrarMenu = document.querySelector(".cerrarMenu");
+
+
+// boton.onclick = function() {
+//     menu.classList.toggle("mostrar");
+// }
+
+
+let menuNav = document.querySelector(".nav-lista");
+let botonNav = document.querySelector(".nav-boton");
+
+botonNav.onclick = function() {
+    menuNav.classList.toggle("mostrar");
+}
